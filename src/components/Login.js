74cc22666be8c355a7de49d61d6bb66836a01fd1
NathanB09 <nathanbell09@gmail.com>
2019-02-21 15:41:57 +0000

    added login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { SIGNUP } from '../constants/routes'
 
-const Login = () => {
-  return (
-    <div>
-      in login
-    </div>
-  );
-};
+class Login extends Component {
+
+  state = {
+    email: '',
+    password: ''
+  }
+
+  handleSubmit = (e) => {
+
+  }
+
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  render() {
+    const { email, password } = this.state
+
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            onChange={this.handleChange}
+            type="text"
+            name='email'
+            placeholder='Email' value={email}
+          />
+          <input
+            onChange={this.handleChange}
+            type="password"
+            name='password'
+            placeholder='Password'
+            value={password}
+          />
+          <button type='submit'>Login</button>
+        </form>
+
+        <div>
+          <p>Don't have an account? <Link to={SIGNUP}>Sign Up</Link></p>
+        </div>
+      </div>
+    )
+  }
+}
 
 export default Login;
