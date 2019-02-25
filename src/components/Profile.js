5522562362e4af558a209ed43decfe5062f35538
NathanB09 <nathanbell09@gmail.com>
2019-02-25 22:04:24 +0000

    profile noew renders users information

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -6,20 +6,52 @@ import * as ROUTES from '../constants/routes'
 class Profile extends Component {
 
   state = {
-    username: ''
+    username: '',
+    budgetMin: '',
+    budgetMax: '',
+    drink: '',
+    drugs: '',
+    smoke: '',
+    profile_img: ''
   }
 
   componentDidMount() {
     const user = this.props.firebase.auth.currentUser
     this.props.firebase.user(user.uid)
-      .on('value', snapshot => this.setState({ username: snapshot.val().username }))
+      .on('value', snapshot => this.setState({
+        username: snapshot.val().username,
+        budgetMin: snapshot.val().budgetMin,
+        budgetMax: snapshot.val().budgetMax,
+        drink: snapshot.val().drink,
+        drugs: snapshot.val().drugs,
+        smoke: snapshot.val().smoke,
+        profile_img: snapshot.val().profile_img
+      }))
   }
 
+  componentWillUnmount() {
+    const user = this.props.firebase.auth.currentUser
+    this.props.firebase.user(user.uid).off()
+  }
+
+
   render() {
+    const { username,
+      budgetMin,
+      budgetMax,
+      drink,
+      drugs,
+      smoke,
+      profile_img } = this.state
     return (
       <div>
         <h1>Profile</h1>
-        <h2>{this.state.username}</h2>
+        <img src={profile_img} alt="" />
+        <h2>{username}</h2>
+        <p>Budget: £{budgetMin} - £{budgetMax}</p>
+        <p>Drinks: {drink}</p>
+        <p>Drugs: {drugs}</p>
+        <p>Smokes: {smoke}</p>
         <Link to={ROUTES.EDIT_PROFILE}><button>Edit Profile</button></Link>
       </div >
     );
