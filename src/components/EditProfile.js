bb8c35fdc89f0f13a939f1c36b5df0efa2a30287
NathanB09 <nathanbell09@gmail.com>
2019-02-25 22:03:57 +0000

    initial form setup and functionality for edit profile

--- src/components/EditProfile.js
+++ src/components/EditProfile.js
@@ -1,39 +1,133 @@
 import React, { Component } from 'react';
 import { withAuthorization } from '../session';
+import * as ROUTES from '../constants/routes'
+
+const INITIAL_STATE = {
+  fileSelect: null,
+  fileName: '',
+  metadata: {},
+  budgetMin: '',
+  budgetMax: '',
+  drink: '-',
+  drugs: '-',
+  smoke: '-',
+  error: null
+}
 
 class EditProfile extends Component {
 
   state = {
-    fileSelect: null,
-    fileName: '',
-    metadata: {}
+    ...INITIAL_STATE
   }
 
-  handleChange = (e) => {
+  componentDidMount() {
+    const user = this.props.firebase.auth.currentUser
+    this.props.firebase.user(user.uid)
+      .on('value', snapshot =>
+        snapshot.val().budgetMin &&
+        snapshot.val().budgetMax &&
+        snapshot.val().drink &&
+        snapshot.val().drugs &&
+        snapshot.val().smoke &&
+        this.setState({
+          budgetMin: snapshot.val().budgetMin,
+          budgetMax: snapshot.val().budgetMax,
+          drink: snapshot.val().drink,
+          drugs: snapshot.val().drugs,
+          smoke: snapshot.val().smoke
+        }))
+  }
+
+  componentWillUnmount() {
+    const user = this.props.firebase.auth.currentUser
+    this.props.firebase.user(user.uid).off()
+  }
+
+  handleImageChange = (e) => {
     const file = e.target.files[0]
-    this.setState({
-      fileSelect: file,
-      fileName: file.name,
-      metadata: { contentType: file.type }
-    })
-    const reader = new FileReader()
-    reader.onload = e => document.querySelector('#preview').src = e.target.result
-    reader.readAsDataURL(file)
+    if (file) {
+      this.setState({
+        fileSelect: file,
+        fileName: file.name,
+        metadata: { contentType: file.type }
+      })
+      const reader = new FileReader()
+      reader.onload = e => document.querySelector('#preview').src = e.target.result
+      reader.readAsDataURL(file)
+    }
+  }
+
+  handlePreferenceChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value })
   }
 
   handleSubmit = (e) => {
     e.preventDefault()
-    const { fileSelect, fileName, metadata } = this.state
-    this.props.firebase.upload(fileSelect, fileName, metadata)
-      .then(snapshot => console.log(snapshot.ref.getDownloadURL()))
+    const {
+      fileSelect,
+      fileName,
+      metadata,
+      budgetMax,
+      budgetMin,
+      drink,
+      drugs,
+      smoke } = this.state
+    const user = this.props.firebase.auth.currentUser
+
+    fileName && this.props.firebase.upload(fileSelect, fileName, metadata)
+      .then(snapshot => snapshot.ref.getDownloadURL())
+      .then(profile_img => {
+        this.props.firebase
+          .user(user.uid)
+          .update({ budgetMin, budgetMax, drink, drugs, smoke, profile_img })
+      })
+      .then(() => {
+        this.setState({ ...INITIAL_STATE })
+        this.props.history.push(ROUTES.PROFILE)
+      })
+      .catch(error => this.setState({ error }))
   }
 
   render() {
+    const { fileName, budgetMin, budgetMax, drink, drugs, smoke, error } = this.state
     return (
       <form onSubmit={this.handleSubmit}>
-        <img id="preview" src="#" alt="your preview" />
-        <input onChange={this.handleChange} type="file" accept="image/*" />
-        <input type="submit" />
+        {fileName && <img id="preview" src="#" alt="your preview" />}
+        <input
+          onChange={this.handleImageChange}
+          type="file"
+          accept="image/*" />
+        <input
+          onChange={this.handlePreferenceChange}
+          type="text"
+          name="budgetMin"
+          placeholder="Budget min"
+          value={budgetMin} />
+        <input
+          onChange={this.handlePreferenceChange}
+          type="text"
+          name="budgetMax"
+          placeholder="Budget max"
+          value={budgetMax} />
+        <select onChange={this.handlePreferenceChange} name="drink">
+          <option defaultValue>{drink}</option>
+          <option value="no">No</option>
+          <option value="socially">Socially</option>
+        </select>
+        <select onChange={this.handlePreferenceChange} name="drugs">
+          <option defaultValue>{drugs}</option>
+          <option value="no">No</option>
+          <option value="sometimes">Sometimes</option>
+          <option value="often">Often</option>
+        </select>
+        <select onChange={this.handlePreferenceChange} name="smoke">
+          <option defaultValue>{smoke}</option>
+          <option value="no">No</option>
+          <option value="sometimes">Sometimes</option>
+          <option value="often">Often</option>
+        </select>
+        <input type="submit" value="Save" />
+        {error && <p>{error.message}</p>}
       </form >
     );
   }
