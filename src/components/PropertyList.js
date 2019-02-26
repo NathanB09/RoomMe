ca6b70afcaa969240ca83e3252afb7a6d8ad0ed7
NathanB09 <nathanbell09@gmail.com>
2019-02-26 16:09:07 +0000

    properties are now listed based on users budget

--- src/components/PropertyList.js
+++ src/components/PropertyList.js
@@ -6,32 +6,55 @@ import Property from './Property'
 class PropertyList extends React.Component {
 
   state = {
+    currentUser: {},
     properties: null,
     pageNo: 1
   }
 
   componentDidMount() {
-    API.testCall(this.state.pageNo).then(data => this.setState({ properties: data }))
+    const userId = this.props.firebase.auth.currentUser.uid
+    this.props.firebase.users().on('value', snapshot => {
+      this.setState({ currentUser: snapshot.val()[userId] })
+      const { currentUser, pageNo } = this.state
+      API.getProperties(currentUser.budgetMin, currentUser.budgetMax, pageNo)
+        .then(data => this.setState({ properties: data }))
+    })
   }
 
   nextPage = async () => {
     await this.setState({ pageNo: this.state.pageNo + 1 })
-    API.testCall(this.state.pageNo).then(data => this.setState({ properties: data }))
+    const { currentUser, pageNo } = this.state
+    API.getProperties(currentUser.budgetMin, currentUser.budgetMax, pageNo)
+      .then(data => this.setState({ properties: data }))
   }
 
   prevPage = async () => {
     await this.setState({ pageNo: this.state.pageNo - 1 })
-    API.testCall(this.state.pageNo).then(data => this.setState({ properties: data }))
+    const { currentUser, pageNo } = this.state
+    API.getProperties(currentUser.budgetMin, currentUser.budgetMax, pageNo)
+      .then(data => this.setState({ properties: data }))
   }
 
   render() {
-    const { properties } = this.state
+    const { properties, pageNo } = this.state
     return (
       <div>
         <div>
-          <button onClick={this.prevPage}>prev page</button>
-          <button onClick={this.nextPage}>next page</button>
-          {properties && <p>Page {properties.response.page} of {properties.response.total_pages}</p>}
+          {properties &&
+            <button
+              onClick={this.prevPage}
+              disabled={pageNo === 1 ? true : false}>
+              prev page
+            </button>}
+          {properties &&
+            <button
+              onClick={this.nextPage}
+              disabled={pageNo === properties.response.total_pages ? true : false}>
+              next page
+            </button>}
+          {properties &&
+            <p>Page {properties.response.page} of {properties.response.total_pages}</p>
+          }
         </div>
         <div>
           {properties && properties.response.listings.map(listing => <Property key={listing.lister_url} listing={listing} />)}
