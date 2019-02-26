e942bfc6a5995b7aaea053a264e624a82c5fd1b5
NathanB09 <nathanbell09@gmail.com>
2019-02-26 19:54:57 +0000

    changed the number of property items fetched to 5

--- src/constants/API.js
+++ src/constants/API.js
@@ -1,7 +1,7 @@
 class API {
   static init() {
     const proxyurl = "https://cors-anywhere.herokuapp.com/"
-    this.baseURL = proxyurl + 'https://api.nestoria.co.uk/api?encoding=json&action=search_listings&country=uk&listing_type=rent&place_name=london&bedroom_min=2&bathroom_min=1&number_of_results=10'
+    this.baseURL = proxyurl + 'https://api.nestoria.co.uk/api?encoding=json&action=search_listings&country=uk&listing_type=rent&place_name=london&bedroom_min=2&bathroom_min=1&number_of_results=5'
     this.budgetURL = (min, max) => `${this.baseURL}&price_min=${min}&price_max=${max}`
   }
 
