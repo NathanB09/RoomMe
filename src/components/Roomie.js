d6d3b8eba66d1017039442ca58cc2d9a24b9810e
NathanB09 <nathanbell09@gmail.com>
2019-02-26 16:09:40 +0000

    added roomie component to display info

---
+++ src/components/Roomie.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Roomie = ({ roomie }) => {
+  return (
+    <div>
+      <img src={roomie.profile_img} alt="roomie pic" />
+      <h2>{roomie.username}</h2>
+      <p>Budget: £{roomie.budgetMin} - £{roomie.budgetMax} pcm</p>
+      <p>Drinks: {roomie.drink}</p>
+      <p>Drugs: {roomie.drugs}</p>
+      <p>Smoke: {roomie.smoke}</p>
+    </div>
+  );
+};
+
+export default Roomie;
