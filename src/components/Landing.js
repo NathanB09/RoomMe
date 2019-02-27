a51212db854adc3d21fdb4229c05715b1d2a8475
NathanB09 <nathanbell09@gmail.com>
2019-02-27 11:47:26 +0000

    landing page logo added with styling

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -4,7 +4,7 @@ import '../css/Landing.css'
 const Landing = () => {
   return (
     <main className="landing_page">
-      <img src="./landing_logo.svg" alt="roomie logo" />
+      <img src={require("../images/landing_logo.svg")} alt="roomie logo" />
     </main>
   );
 };
