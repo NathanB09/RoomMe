bb19c44a1fc6868826fb8a279ba87a5c15b33f4f
NathanB09 <nathanbell09@gmail.com>
2019-03-05 17:30:57 +0000

    extra wrapper added to roomie card for styling

--- src/components/Roomie.js
+++ src/components/Roomie.js
@@ -3,30 +3,32 @@ import { Link } from 'react-router-dom'
 
 const Roomie = ({ roomie }) => {
   return (
-    <Link to={`/roomies/${roomie.username}`} className="roomie_card">
-      <div className="roomie_pic_wrapper">
-        <img src={roomie.profile_img} alt="roomie pic" />
-        <h2>{roomie.username}</h2>
-      </div>
-      <div className="roomie_details_wrapper">
-        <p>
-          <i className="fas fa-coins"></i>
-          £{roomie.budgetMin} - £{roomie.budgetMax} pcm
+    <div className={"roomie_card_wrapper"}>
+      <Link to={`/roomies/${roomie.username}`} className="roomie_card">
+        <div className="roomie_pic_wrapper">
+          <img src={roomie.profile_img} alt="roomie pic" />
+          <h2>{roomie.username}</h2>
+        </div>
+        <div className="roomie_details_wrapper">
+          <p>
+            <i className="fas fa-coins"></i>
+            £{roomie.budgetMin} - £{roomie.budgetMax} pcm
         </p>
-        <p>
-          <i className="fas fa-cocktail"></i>
-          Drinks: {roomie.drink}
-        </p>
-        <p>
-          <i className="fas fa-pills"></i>
-          Drugs: {roomie.drugs}
-        </p>
-        <p>
-          <i className="fas fa-smoking"></i>
-          Smoke: {roomie.smoke}
-        </p>
-      </div>
-    </Link>
+          <p>
+            <i className="fas fa-cocktail"></i>
+            Drinks: {roomie.drink}
+          </p>
+          <p>
+            <i className="fas fa-pills"></i>
+            Drugs: {roomie.drugs}
+          </p>
+          <p>
+            <i className="fas fa-smoking"></i>
+            Smoke: {roomie.smoke}
+          </p>
+        </div>
+      </Link>
+    </div>
   );
 };
 
