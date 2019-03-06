310769e913d3047a2e5edbac74b62e14257e5d13
NathanB09 <nathanbell09@gmail.com>
2019-03-06 20:28:47 +0000

    added more info to the landing page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -5,6 +5,10 @@ const Landing = () => {
   return (
     <main className="landing_page">
       <img src={require("../images/landing_logo.svg")} alt="roomie logo" />
+      <div>
+        <p>Find roommates looking to rent within your price range</p>
+        <p>Roomate matches based on your preferences</p>
+      </div>
     </main>
   );
 };
